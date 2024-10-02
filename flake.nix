cbfb9b7d09a0e4c956d7052eca9cd75698e6054b
Lenivaya <Lenivaya@users.noreply.github.com>
2024-10-02 12:38:18 +0000

    Apply formatting changes

--- flake.nix
+++ flake.nix
@@ -6,56 +6,51 @@
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    inputs@{
-      nixpkgs,
-      flake-parts,
-      treefmt-nix,
-      ...
-    }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    treefmt-nix,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
         treefmt-nix.flakeModule
       ];
 
       systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem =
-        {
-          pkgs,
-          self',
-          lib,
-          ...
-        }:
-        {
-          devShells =
-            let
-              bareMinimum = with pkgs; [
-                corepack
-                just
-              ];
-            in
-            {
-              default = pkgs.mkShell {
-                nativeBuildInputs = bareMinimum;
-              };
+      perSystem = {
+        pkgs,
+        self',
+        lib,
+        ...
+      }: {
+        devShells = let
+          bareMinimum = with pkgs; [
+            corepack
+            just
+          ];
+        in {
+          default = pkgs.mkShell {
+            nativeBuildInputs = bareMinimum;
+          };
 
-              ci = pkgs.mkShell {
-                nativeBuildInputs = with pkgs; [
-                  corepack
-                  nodejs_22
-                ];
-              };
-            };
-
-          treefmt = {
-            projectRootFile = "flake.nix";
-
-            programs = {
-              alejandra.enable = true;
-              # Manage using ci instead
-              # prettier.enable = true;
-            };
+          ci = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              corepack
+              nodejs_22
+            ];
           };
         };
+
+        treefmt = {
+          projectRootFile = "flake.nix";
+
+          programs = {
+            alejandra.enable = true;
+            # Manage using ci instead
+            # prettier.enable = true;
+          };
+        };
+      };
     };
 }
